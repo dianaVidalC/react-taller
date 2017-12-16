be5707f7265f022d6cd7f1a4f412c52e7731e241
glisselisbeth <glisselisbeth@gmail.com>
2017-12-16 14:45:40 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -7,9 +7,28 @@ import ProductRow from './components/ProductRow.jsx';
 
 var arrayProductos = [{nombre:'computadora', precio:'1000'},
                     {nombre:'impresora', precio:'500'},
-                    {nombre:'escanner', precio:'600'}]
+                    {nombre:'escanner', precio:'600'}];
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: ''
+        };
+
+        this.onChangeValue = this.onChangeValue.bind(this);
+    }
+
+    onChangeValue(ev){
+        debugger;
+        var nombre = ev.target.value;
+
+        this.setState({
+            name:nombre
+        })
+    }
+
   render() {
     return (
       <div className="App">
@@ -21,6 +40,12 @@ class App extends Component {
           <ProductForm
           productos={ arrayProductos }
           />
+          <br/>
+          Nombre: <input type='text' placeholder='Ingresar nombre...' onChange={this.onChangeValue}/>
+          <br/>
+          <br/>
+          <br/>
+          <label style={{textAlign:'center', color:'green'}}>{this.state.name}</label>
       </div>
     );
   }
